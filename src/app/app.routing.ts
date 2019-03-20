1ebc73986389f8ea3d035f842df6918c69fda063
Elvis Alcántara Pinedo <aelvis@users.noreply.github.com>
2019-03-20 10:11:43 -0500

    pedido v1
    update

---
+++ src/app/app.routing.ts
@@ -0,0 +1,20 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+//Admin
+import { InicioComponent} from './component/inicio/inicio.component';
+import { LoginComponent} from './component/login/login.component';
+import { PedidoComponent } from './component/pedido/pedido.component';
+
+import { AdminGuard } from './services/admin.guard';
+
+const appRoutes: Routes = [
+	{path: '', component: LoginComponent},
+	{path: '', redirectTo: 'login', pathMatch: 'full'},
+	{path: 'login', component: LoginComponent},
+	{path: 'inicio', component: InicioComponent, canActivate: [AdminGuard]},
+	{path: 'pedido', component: PedidoComponent, canActivate: [AdminGuard]},
+	{path: '**', component: LoginComponent},
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, { enableTracing: true, useHash:true });
